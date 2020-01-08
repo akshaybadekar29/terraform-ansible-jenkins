46b2cc0563b8c2bdbef17e014424b5ccb1160e80
akshaybadekar29 <akshaybadekar29@gmail.com>
2020-01-08 07:51:54 +0530

    Added vars.tf

---
+++ vars.tf
@@ -0,0 +1,30 @@
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+variable "aws_region" {
+    description = "EC2 Region for the VPC"
+    default = "eu-west-1"
+}
+
+variable "vpc_cidr" {
+    description = "CIDR for the whole VPC"
+    default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.0.0/24"
+}
+
+variable "public_subnet_2_cidr" {
+    description = "CIDR for the Public Subnet"
+    default = "10.0.2.0/24"
+}
+
+
+variable "private_subnet_cidr" {
+    description = "CIDR for the Private Subnet"
+    default = "10.0.1.0/24"
+}
